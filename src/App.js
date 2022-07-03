63fc2ce317d04241119604083b6120efe091f479
Samir Santos <santossamir@hotmail.com>
2022-07-02 23:20:06 -0300

    Iniciando o app e configurando o Firebase.

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import firebase from "./services/FirebaseConnection";
+
+export default function App() {
+  return (
+    <div className="App">
+        <h1>Hello, people!</h1>
+    </div>
+  );
+}
+
+
---
+++ src/services/FirebaseConnection.js
@@ -0,0 +1,18 @@
+import firebase from "firebase/app";
+import 'firebase/auth';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC_4djbDstybI86sNt_J99U1E-NW0AlrbM",
+    authDomain: "sistemachamados-503ae.firebaseapp.com",
+    projectId: "sistemachamados-503ae",
+    storageBucket: "sistemachamados-503ae.appspot.com",
+    messagingSenderId: "192503654601",
+    appId: "1:192503654601:web:d2318bf953cf2426004518",
+    measurementId: "G-XQFCRRDT8T"
+  };
+
+  if(!firebase.apps.length){
+    firebase.initializeApp(firebaseConfig);
+  }
+
+  export default firebase;
