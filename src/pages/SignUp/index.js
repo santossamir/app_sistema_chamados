4fffdb3df5630994e0e330eb8ee6efc0a505706b
Samir Santos <santossamir@hotmail.com>
2022-07-03 12:42:28 -0300

    Desenvolvendo a page de cadastro.

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,11 +1,34 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../assets/logo.png';
 
 export default function SignUp() {
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassworld] = useState('');
+
+  function handleSubmit(e){
+    e.preventDefault();
+    alert('Clicou!');
+  }
+
     return (
-      <div className="App">
-          <h1>Registration page!</h1>
+      <div className="containerCenter">
+        <div className='login'>
+          <div className='loginArea'>
+            <img src={logo} alt="Sistema Logo"/>
+          </div>
+          <form onSubmit={ handleSubmit }>
+            <h1>Cadastrar Conta</h1>
+            <input type="text" placeholder="Seu nome" value={nome} onChange={(e) => setNome(e.target.value)} />
+            <input type="text" placeholder='email@email.com' value={email} onChange={(e) => setEmail(e.target.value)}/>
+            <input type="password" placeholder='******' value={password} onChange={(e) => setPassworld(e.target.value)} />
+            <button type="submit">Cadastrar</button>
+          </form>
+          <Link to="/">Já tem uma conta? Clique aqui.</Link>
+        </div>
       </div>
     );
   }
-  
   
   
