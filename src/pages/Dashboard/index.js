3115419ea94c40381cec3f2be82e37e405e6d7c9
Samir Santos <santossamir@hotmail.com>
2022-07-06 14:32:54 -0300

    Desenvolvedo a page dashboard.

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,13 +1,37 @@
-import { useContext } from "react";
-import { AuthContext } from "../../contexts/auth";
+import { useState } from "react";
 import Header from '../../components/Header';
+import Title from "../../components/Title";
+import { FiMessageSquare, FiPlus } from "react-icons/fi";
+import { Link } from "react-router-dom";
+import "./dashboard.css";
 
 export default function Dashboard(){
-    const { signOut } = useContext(AuthContext);
+    const [chamados, setChamados] = useState([]);
+   
     return(
         <div>
             <Header/>
-            <h1>Dashboard page!</h1>
+            <div className="content">
+                <Title name="Atendimentos">
+                    <FiMessageSquare size={25}/>
+                </Title>
+                {chamados.length === 0 ? (
+                    <div className="container deshboard">
+                        <span>Nenhum chamado registrado...</span>
+                        <Link to="/new" className="new">
+                            <FiPlus size={25} color="#FFF"/>
+                            Novo chamado
+                        </Link>
+                    </div>
+                ):(
+                    <>
+                        <Link to="/new" className="new">
+                            <FiPlus size={25} color="#FFF"/>
+                            Novo chamado
+                        </Link>
+                    </>
+                )}
+            </div>
             
         </div>
     )
